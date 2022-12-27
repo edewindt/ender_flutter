14e0b33371b9331ac54cb26310bfd6a6ca72e935
edewindt <edewindt27@outlook.com>
2022-12-27 02:34:14 -0600

    drawer items added

--- lib/widgets/drawer_s.dart
+++ lib/widgets/drawer_s.dart
@@ -5,8 +5,32 @@ class Drawer_s extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget buildItem(String title, IconData icon) {
+      return ListTile(
+        leading: Icon(icon),
+        title: Text(title),
+      );
+    }
+
     return Drawer(
-      child: Text('Drawer initiated'),
+      backgroundColor: Theme.of(context).shadowColor,
+      child: Column(
+        children: [
+          Container(
+            height: 100,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.center,
+            child: Text(
+              'Ender API',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          buildItem('Armies', Icons.colorize),
+          buildItem('Species', Icons.public),
+          buildItem('Characters', Icons.groups)
+        ],
+      ),
     );
   }
 }

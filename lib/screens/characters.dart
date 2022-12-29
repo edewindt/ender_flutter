71abb3d3a67995ce8e45f2cbd55e852c4aab1b2a
edewindt <edewindt27@outlook.com>
2022-12-29 13:08:34 -0600

    rendering api data adding loading

--- lib/screens/characters.dart
+++ lib/screens/characters.dart
@@ -1,8 +1,8 @@
 import 'package:ender_app/screens/loading.dart';
 import 'package:ender_app/widgets/card_i.dart';
 import 'package:flutter/material.dart';
-
-import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Characters_s extends StatefulWidget {
   Characters_s({super.key});
@@ -15,29 +15,52 @@ class Characters_s extends StatefulWidget {
 
 class _Characters_sState extends State<Characters_s> {
   List items = [];
+  bool _isLoading = false;
+  Future<void> readJson() async {
+    setState(() {
+      _isLoading = true;
+    });
+    final _url = "https://enderapi-production.up.railway.app/characters";
+    http.Response response = await http.get(Uri.parse(_url));
+    // final String response = await rootBundle.loadString('data/data.json');
+    final data = await json.decode(response.body);
+
+    setState(() {
+      items = data;
+      _isLoading = false;
+    });
+
+// ...
+  }
 
   @override
+  void initState() {
+    readJson();
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
-    return Loading();
-    // return GridView(
-    //   children: [
-    //     for (var i in items)
-    //       CardItem(
-    //         id: i['id'],
-    //         title: i['name'],
-    //         image: i['media'],
-    //         bio: i['bio'],
-    //         species: i['species'],
-    //         trvia: i['trivia'],
-    //         quote: i['quote'],
-    //       )
-    //   ],
-    //   gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-    //     maxCrossAxisExtent: 300,
-    //     childAspectRatio: 3 / 2,
-    //     crossAxisSpacing: 20,
-    //     mainAxisSpacing: 20,
-    //   ),
-    // );
+    return _isLoading
+        ? Loading()
+        : GridView(
+            children: [
+              for (var i in items)
+                CardItem(
+                  id: i['id'],
+                  title: i['name'],
+                  image: i['media'],
+                  bio: i['bio'],
+                  species: i['species'],
+                  trvia: i['trivia'],
+                  quote: i['quote'],
+                )
+            ],
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 300,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+            ),
+          );
   }
 }
